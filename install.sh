ef959d8d281eba70c3892f5dd983618aa2680a5f
Christopher Wellons <mosquitopsu@gmail.com>
2012-06-01 22:44:42 -0400

    Adjust permissions in installed files.

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ do
     dotfile=${file/_/.}
     echo Installing $dotfile
     if [ ! -e $(dirname ~/$dotfile) ]; then
-        mkdir -p $(dirname ~/$dotfile)
+        mkdir -p -m 700  $(dirname ~/$dotfile)
     fi
     ln -fs $(pwd)/$file ~/$dotfile
+    chmod go-rwx $file
 done
 
 ln -sf /dev/null ~/.bash_history
