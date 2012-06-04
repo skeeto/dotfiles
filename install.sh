60cd3868c63eee61a388e90b1e1189de60001d19
Christopher Wellons <mosquitopsu@gmail.com>
2012-06-04 01:16:01 -0400

    Add support for private (encrypted) dotfiles.

--- install.sh
+++ install.sh
@@ -2,13 +2,29 @@
 
 find -regex "./_.*" -type f -print0 | sort -z | while read -d $'\0' file
 do
-    dotfile=${file/_/.}
-    echo Installing $dotfile
+    dotfile=${file/.\/_/.}
+    decfile=${dotfile/.priv.gpg/}
+
+    ## Install directory first
     if [ ! -e $(dirname ~/$dotfile) ]; then
         mkdir -p -m 700  $(dirname ~/$dotfile)
     fi
-    ln -fs $(pwd)/$file ~/$dotfile
-    chmod go-rwx $file
+
+    ## Install the file
+    if [ ${file#./*.} = priv.gpg ]; then
+        ## Decrypt into place
+        if [ $file -nt ~/$decfile ]; then
+            echo Decrypting ~/$decfile
+            gpg --quiet --batch --yes --decrypt --output ~/$decfile $file
+        else
+            echo Skipping $dotfile
+        fi
+    else
+        ## Create a link to the repository version
+        echo Installing $dotfile
+        ln -fs $(pwd)/$file ~/$dotfile
+        chmod go-rwx $file
+    fi
 done
 
 ln -sf /dev/null ~/.bash_history
