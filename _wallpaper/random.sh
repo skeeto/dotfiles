b9a30b9e422e27ef9f99967364a351d2db2a9b7a
Christopher Wellons <mosquitopsu@gmail.com>
2012-06-04 22:58:07 -0400

    General random wallpaper script name.

--- _wallpaper/random.sh
+++ _wallpaper/random.sh
@@ -2,10 +2,11 @@
 
 DELAY=600
 DIR=~/.wallpaper
+SHOW="feh --bg-fill"
 
 find $DIR -type f -print0 | shuf -z | while read -d $'\0' image
 do
-    feh --bg-fill "$image"
+    $SHOW "$image"
     if [ "$1" == --once ]; then exit; fi
     sleep $DELAY
 done
