a4ffb15af2ff11eed3260d75df89a66a3671b10a
Christopher Wellons <mosquitopsu@gmail.com>
2013-06-16 16:36:01 -0400

    Infinite random wallpaper loop.

--- _wallpaper/random.sh
+++ _wallpaper/random.sh
@@ -4,9 +4,12 @@ DELAY=600
 DIR=~/.wallpaper
 SHOW="feh --bg-fill"
 
-find $DIR -type f -print0 | shuf -z | while read -d $'\0' image
+while true
 do
-    $SHOW "$image"
-    if [ "$1" == --once ]; then exit; fi
-    sleep $DELAY
+    find $DIR -type f -print0 | shuf -z | while read -d $'\0' image
+    do
+        $SHOW "$image"
+        if [ "$1" == --once ]; then exit; fi
+        sleep $DELAY
+    done
 done
