c76074fa344b8451cec00a0f04a83ac6441f5b01
Christopher Wellons <wellons@nullprogram.com>
2017-03-06 22:19:20 -0500

    Explicitly read tar from stdin in random.sh.

--- _wallpaper/fetch.sh
+++ _wallpaper/fetch.sh
@@ -4,5 +4,5 @@
 set -e -- kde.tar misc.tar 100-wallpapers.tar large-set.tar
 base=http://skeeto.s3.amazonaws.com/wallpapers
 for tar; do
-    curl "$base/$tar" | tar -xv
+    curl "$base/$tar" | tar -xvf -
 done
