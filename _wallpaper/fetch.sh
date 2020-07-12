677763fbcfd103747a4e744605f471c14f0b2ac0
Christopher Wellons <wellons@nullprogram.com>
2020-07-12 12:39:06 -0400

    Add my wife's wallpaper set

--- _wallpaper/fetch.sh
+++ _wallpaper/fetch.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-## Fetches my personal collection of landscape wallpapers.
+## Fetches my personal collection of nature wallpapers.
 
-set -e -- kde.tar misc.tar 100-wallpapers.tar large-set.tar
+set -e -- \
+    kde.tar \
+    misc.tar \
+    100-wallpapers.tar \
+    large-set.tar \
+    Kelsey-Wellons-Wallpaper-Collection-1.tar
 base=http://skeeto.s3.amazonaws.com/wallpapers
 for tar; do
     curl "$base/$tar" | tar -xvf -
