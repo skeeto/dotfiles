60bf100872b708bf71358d4d655301dba74db383
Christopher Wellons <wellons@nullprogram.com>
2020-07-27 12:16:55 -0400

    Dump all the old wallapers and only use my wife's

--- _wallpaper/fetch.sh
+++ _wallpaper/fetch.sh
@@ -2,11 +2,8 @@
 ## Fetches my personal collection of nature wallpapers.
 
 set -e -- \
-    kde.tar \
-    misc.tar \
-    100-wallpapers.tar \
-    large-set.tar \
-    Kelsey-Wellons-Wallpaper-Collection-1.tar
+    Kelsey-Wellons-Wallpaper-Collection-1.tar \
+    Kelsey-Wellons-Wallpaper-Collection-2.tar
 base=http://skeeto.s3.amazonaws.com/wallpapers
 for tar; do
     curl "$base/$tar" | tar -xvf -
