45e051b513c28767a4c1a0a9ff4d7a340f0b5b54
Christopher Wellons <wellons@nullprogram.com>
2020-10-29 18:28:54 -0400

    Add fourth wallpaper collection

--- _wallpaper/fetch.sh
+++ _wallpaper/fetch.sh
@@ -4,7 +4,8 @@
 set -e -- \
     Kelsey-Wellons-Wallpaper-Collection-1.tar \
     Kelsey-Wellons-Wallpaper-Collection-2.tar \
-    Kelsey-Wellons-Wallpaper-Collection-3.tar
+    Kelsey-Wellons-Wallpaper-Collection-3.tar \
+    Kelsey-Wellons-Wallpaper-Collection-4.tar
 base=http://skeeto.s3.amazonaws.com/wallpapers
 for tar; do
     curl "$base/$tar" | tar -xvf -
