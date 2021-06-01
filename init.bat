1f6462b118ca8a9ac6dcc416f5a39ef743cc89c8
Christopher Wellons <wellons@nullprogram.com>
2021-06-01 19:59:11 -0400

    Remove more annoyances in init.bat

--- init.bat
+++ init.bat
@@ -6,17 +6,21 @@
 
 :: Annoyances
 set key="HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-reg add "%key%" /f /v TaskbarGlomLevel         /t REG_DWORD /d 2
-reg add "%key%" /f /v HideFileExt              /t REG_DWORD /d 0
-reg add "%key%" /f /v ShowCortanaButton        /t REG_DWORD /d 0
-reg add "%key%" /f /v ShowTaskViewButton       /t REG_DWORD /d 0
-reg add "%key%" /f /v StoreAppsOnTaskbar       /t REG_DWORD /d 0
-reg add "%key%" /f /v MultiTaskingAltTabFilter /t REG_DWORD /d 3
-reg add "%key%" /f /v JointResize              /t REG_DWORD /d 0
-reg add "%key%" /f /v SnapFill                 /t REG_DWORD /d 0
-reg add "%key%" /f /v SnapAssist               /t REG_DWORD /d 0
+reg add "%key%" /f /v TaskbarGlomLevel          /t REG_DWORD /d 2
+reg add "%key%" /f /v HideFileExt               /t REG_DWORD /d 0
+reg add "%key%" /f /v ShowCortanaButton         /t REG_DWORD /d 0
+reg add "%key%" /f /v ShowTaskViewButton        /t REG_DWORD /d 0
+reg add "%key%" /f /v StoreAppsOnTaskbar        /t REG_DWORD /d 0
+reg add "%key%" /f /v MultiTaskingAltTabFilter  /t REG_DWORD /d 3
+reg add "%key%" /f /v JointResize               /t REG_DWORD /d 0
+reg add "%key%" /f /v SnapFill                  /t REG_DWORD /d 0
+reg add "%key%" /f /v SnapAssist                /t REG_DWORD /d 0
 set key="HKCU\Software\Microsoft\Windows\CurrentVersion\Feeds"
 reg add "%key%" /f /v ShellFeedsTaskbarViewMode /t REG_DWORD /d 2
+set key="HKCU\Software\Microsoft\Windows\CurrentVersion\Search"
+reg add "%key%" /f /v SearchboxTaskbarMode      /t REG_DWORD /d 0
+set key="HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband"
+reg delete "%key%" /f /v Favorites
 
 :: Paths
 setx GOBIN "%USERPROFILE%\bin"
