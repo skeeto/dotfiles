ddd1e7f1fb1dd60120ed690e1813c8dfa112028c
Christopher Wellons <wellons@nullprogram.com>
2023-04-12 19:19:28 -0400

    Add tenth wallpaper collection

--- _wallpaper/fetch.sh
+++ _wallpaper/fetch.sh
@@ -2,15 +2,16 @@
 ## Fetches my personal collection of nature wallpapers.
 
 set -e -- \
-    Kelsey-Wellons-Wallpaper-Collection-1.tar \
-    Kelsey-Wellons-Wallpaper-Collection-2.tar \
-    Kelsey-Wellons-Wallpaper-Collection-3.tar \
-    Kelsey-Wellons-Wallpaper-Collection-4.tar \
-    Kelsey-Wellons-Wallpaper-Collection-5.tar \
-    Kelsey-Wellons-Wallpaper-Collection-6.tar \
-    Kelsey-Wellons-Wallpaper-Collection-7.tar \
-    Kelsey-Wellons-Wallpaper-Collection-8.tar \
-    Kelsey-Wellons-Wallpaper-Collection-9.tar \
+    Kelsey-Wellons-Wallpaper-Collection-1.tar  \
+    Kelsey-Wellons-Wallpaper-Collection-2.tar  \
+    Kelsey-Wellons-Wallpaper-Collection-3.tar  \
+    Kelsey-Wellons-Wallpaper-Collection-4.tar  \
+    Kelsey-Wellons-Wallpaper-Collection-5.tar  \
+    Kelsey-Wellons-Wallpaper-Collection-6.tar  \
+    Kelsey-Wellons-Wallpaper-Collection-7.tar  \
+    Kelsey-Wellons-Wallpaper-Collection-8.tar  \
+    Kelsey-Wellons-Wallpaper-Collection-9.tar  \
+    Kelsey-Wellons-Wallpaper-Collection-10.tar \
 
 base=http://skeeto.s3.amazonaws.com/wallpapers
 for tar; do
